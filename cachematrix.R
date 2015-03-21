6e2748e308a79f2a60427804b3e29de61f60e8fe
an6464 <andreanunez@me.com>
2015-03-21 05:29:57 -0400

    Finished assigment
    Recoded to find inverse of matrix.
    Unit test passed on 3/21/15

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Started with example code from Dr. Peng
 
-## Write a short comment describing this function
+##function requires matrix vector for argument. 
+##reset all variables to NULL
+##take argument given, and store inverse matrix into m at
+##the global (environment) level
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL 
+    set <- function(y) {
+        x <<-y
+        m <<- NULL
+    }
+    #calculate 
+    get <-function() x
+    ##set global variable to inverse of matrix
+    setMatrix <- function(invmatrix) m <<- invmatrix 
+    ##return cached value
+    getMatrix <- function() m
+    list(set = set, get = get,
+         setMatrix = setMatrix,
+         getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+## Started with code example from Dr. Peng
+## changed function to have if, else instead of just if
+
+##check to see if global variable is empty
+##if variable is not empty then grag data
+##if varaible is empty, calculate the inverse of the matrix
+##and return that variable
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getMatrix()
+    if(!is.null(m)) {
+        message("Getting cached data, please wait")
+        return(m)
+    } else {
+        m <- solve(x$get())
+        x$setMatrix(m)
+        return(m)
+    }
 }
